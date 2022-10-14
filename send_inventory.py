04da63d582381d4812bac5418859427f2dc1361d
Amanda Gilmore <ms.amgilmore@gmail.com>
2022-10-14 13:11:06 -0400

    add helper to extract artist ID from body, clean up stub for data payload

--- send_inventory.py
+++ send_inventory.py
@@ -1,24 +1,52 @@
 import json
 import logging
+import re
+import airtable
+
+from os import environ
 from pprint import pprint
 from urllib import response
 
+ID_INDEX = 0
+CATEGORY_INDEX = 1
+DESCRIPTION_INDEX = 2
+PRICE_INDEX = 3
+
+
+def extract_artist_id(janky_string):
+  regex_object = re.search("ID\=\d{5}", janky_string)
+  param = regex_object.group()
+  artist_id = param[3:]
+  return artist_id
 
 def lambda_handler(event, context):
-    payload = clean_up_data(event['multiValueQueryStringParameters'])
-    response = send_to_airtable(payload)
 
+    inventory_as_string = event['multiValueQueryStringParameters'].values()
+    artist = extract_artist_id(event['body'])
+    inventory_items = prepare_payload(artist, inventory_as_string)
+    
+    for each_line in inventory_items:
+      response = send_to_airtable(each_line)
+      if response is not None:
+        # logging.critical(f"an inventory item failed to send to Airtable, inventory ID is {artist_id}-{each_line['Item ID']}.")
+        pass
+    
     return {
-        # TODO replace this boilerplate
-        # return response
         'statusCode': 200,
-        'body': json.dumps("Check your logs")
+        'body': json.dumps("Inventory batch has been sent to the spreadsheet.")
     }
+    
 
+def prepare_payload(artist, query_parameters):
+  """returns a list of dictionaries, each dictionary
+   representing a single line in the spreadsheet to 
+   send to airtable"""
+  payload_dictionary = {}
+    
+  return payload_dictionary
+    
+    
 
-def clean_up_data(raw_data):
-  # TODO
-  return pretty_dictionary
 
 
 def send_to_airtable(data):
