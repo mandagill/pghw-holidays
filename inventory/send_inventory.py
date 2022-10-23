0aeba823b58bdb1013b19b12a636a3ed0cd45569
Amanda Gilmore <ms.amgilmore@gmail.com>
2022-10-23 13:01:08 -0400

    unmarshal json and add unit test

--- inventory/send_inventory.py
+++ inventory/send_inventory.py
@@ -1,56 +1,35 @@
 import json
 import logging
-import re
-from package import airtable
-
-from os import environ
 from pprint import pprint
-from urllib import response
 
-ID_INDEX = 0
-CATEGORY_INDEX = 1
-DESCRIPTION_INDEX = 2
-PRICE_INDEX = 3
+from package import airtable
+from os import environ
 
-
-def extract_artist_id(janky_string):
-  regex_object = re.search("ID\=\d{5}", janky_string)
-  param = regex_object.group()
-  artist_id = param[3:]
-  return artist_id
 
 def lambda_handler(event, context):
-
-    inventory_as_string = event['multiValueQueryStringParameters'].values()
-    artist = extract_artist_id(event['body'])
-    inventory_items = prepare_payload(artist, inventory_as_string)
-    
-    for each_line in inventory_items:
-      response = send_to_airtable(each_line)
-      if response is not None:
-        # logging.critical(f"an inventory item failed to send to Airtable, inventory ID is {artist_id}-{each_line['Item ID']}.")
-        pass
+    inventory = json.loads(event['body']) 
+    inventory_list = prepare_payload(request_body=inventory)
+    send_to_airtable(inventory_list)
     
     return {
         'statusCode': 200,
-        'body': json.dumps("Inventory batch has been sent to the spreadsheet.")
+        'body': json.dumps("Check your logs")
     }
     
 
-def prepare_payload(artist, query_parameters):
-  """returns a list of dictionaries, each dictionary
-   representing a single line in the spreadsheet to 
-   send to airtable"""
-  payload_dictionary = {}
-    
-  return payload_dictionary
+def prepare_payload(request_body):
+  """Create a dictionary per inventory item, return a list of dicts"""
+  # TODO
+  pass
     
 
-def send_to_airtable(table_name, data):
+def send_to_airtable(data):
   """Takes a dictionary and sends it to Airtable, returns a status code from Airtable API"""
-
+  # TODO add error handling 
   at = airtable.Airtable(environ.get("BASE_ID"), environ.get("AIRTABLE_KEY"))
 
-  return at.create(table_name, data)
+  for each_inventory_item in data:
+    at.create("Inventory", each_inventory_item)
   
-
+  return 
+  
--- test/test_send_inventory.py
+++ test/test_send_inventory.py
@@ -1,21 +1,36 @@
-from send_inventory import send_to_airtable
+import unittest
+
+from inventory import send_inventory
 from pprint import pprint
 from random import randint
 
-RANDOM_COLORS = ['', 'red', 'blue', 'green', 'purple', 'yellow', '']
 
-TEST_BODY = "{'body': 'Artist=AmandaTest+GilmoreTest&Item+description=blue+knit+dishtowel%2C+small+woven+wall+hanging&Category+description=towel%2C+tapestry&Taxable='"
+class TestSendInventory(unittest.TestCase):
 
-
-def some_color():
-  return RANDOM_COLORS[randint(1, 5)]
+  RANDOM_COLORS = ['', 'red', 'blue', 'green', 'purple', 'yellow', '']
 
 
-response = send_to_airtable('Inventory',
-  {
-    'Item ID': '12345',
-    'Item description': f"a {some_color()} felted wool scarf",
-    'Category description': 'scarf',
-  })
+  def some_color(self):
+    return self.RANDOM_COLORS[randint(1, 5)]
 
-pprint(response)
+
+  def test_airtable_calls_succeed(self):
+    base_case = {
+      'Item ID': '12345',
+      'Item description': f"a {self.some_color()} felted wool scarf",
+      'Category description': 'scarf',
+    }
+
+    response = send_inventory.send_to_airtable(base_case)
+
+    self.assertNotError(response)
+
+
+  def test_parse_inventory_with_commas(self):
+    unprepared_payload = {'Item description': 'purple scarf woven with hand spun merino, purple woven shirt, spun merino cotton blend, purple, red, blue, orange woven tapestry, 10x2 inches', 'Category description': 'Scarf, Shirt, Wall tapestry', 'Price': '100, 100, 75', 'Name': 'TestUser'}
+
+    list_of_dicts = send_inventory.prepare_payload(unprepared_payload)
+
+    expected = [{'Item ID': 1, 'Artist': 'TestUser', 'Item description': 'purple scarf woven with hand spun merino', 'Category description': 'Scarf', 'Price': 100}, {'Item ID': 2, 'Artist': 'TestUser', 'Item description': 'purple woven shirt, spun merino cotton blend', 'Category description': 'Shirt', 'Price': 100}, {'Item ID': 3, 'Artist': 'TestUser', 'Item description': 'purple, red, blue, orange woven tapestry, 10x2 inches', 'Category description': 'Wall tapestry', 'Price': 75}]
+
+    self.assertEquals(list_of_dicts, expected)
